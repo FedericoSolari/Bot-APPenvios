44cc6c6a8b989b6972024ee3abd41fde1d96b6c2
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-11-15 09:47:20 -0300

    Test: descomentando test

--- spec/bot_client_spec.rb
+++ spec/bot_client_spec.rb
@@ -253,7 +253,7 @@ describe 'BotClient' do
     app.run_once
   end
 
-  xit 'Deberia ver un mensaje de error intentar al crear un nuevo envio sin direccion' do
+  it 'Deberia ver un mensaje de error intentar al crear un nuevo envio sin direccion' do
     cuando_realizo_envio(nil, 'CP:1010', 141_733_544)
     when_i_send_text('fake_token', '/nuevo-envio , CP:1010')
     then_i_get_text('fake_token', 'Verifique que se hayan ingresado todos los parametros (direccion, codigo postal)')
