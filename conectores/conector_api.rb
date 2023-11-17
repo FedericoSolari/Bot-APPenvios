9d0afbc914084241cba9abf00085db8b1115c588
SantiagoLocatelli <slocatelli@fi.uba.ar>
2023-11-17 18:13:17 -0300

    Refactor: poniendo la url de la api como variable de entorno

--- conectores/conector_api.rb
+++ conectores/conector_api.rb
@@ -1,19 +1,9 @@
 require 'faraday'
 require_relative '../excepciones/conexion_api_error'
 
-RUTA_API_TEST = 'http://web:3000'.freeze
-RUTA_API = 'http://restapi:8080'.freeze # Ver el archivo "service.yaml" de la api
-
 class ConectorApi
   def initialize
-    @api_url = case ENV['ENV']
-               when 'test'
-                 RUTA_API_TEST
-               when 'development'
-                 RUTA_API_TEST
-               else
-                 RUTA_API
-               end
+    @api_url = ENV['API_URL'] || ''
   end
 
   def registrar_cliente(nombre, direccion, codigo_postal, id_cliente)
