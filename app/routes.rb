b84394efe35a1d96acf6b240b993ed86939b97ed
FedericoSolari <fsolariv@fi.uba.ar>
2023-11-23 20:29:48 -0300

    Refactor: Cambio de mensaje de comando no encontrado

--- app/routes.rb
+++ app/routes.rb
@@ -13,10 +13,6 @@ class Routes
     bot.api.send_message(chat_id: message.chat.id, text: "Hola, #{message.from.first_name}", parse_mode: 'MarkdownV2')
   end
 
-  on_message_pattern %r{/say_hi (?<name>.*)} do |bot, message, args|
-    bot.api.send_message(chat_id: message.chat.id, text: "Hola, #{args['name']}", parse_mode: 'MarkdownV2')
-  end
-
   on_message_pattern %r{/registrar (?<nombre>.*), (?<direccion>.*), (?<codigo_postal>.*)} do |bot, message, args|
     respuesta = ConectorApi.new.registrar_cliente(args['nombre'], args['direccion'], args['codigo_postal'], message.chat.id)
     formateador = FormateadorRespuesta.new(respuesta)
@@ -86,5 +82,20 @@ class Routes
 
   default do |bot, message|
     bot.api.send_message(chat_id: message.chat.id, text: 'Uh? No te entiendo\\! Me repetis la pregunta?', parse_mode: 'MarkdownV2')
+  end
+
+  default do |bot, message|
+    bot.api.send_message(
+      chat_id: message.chat.id,
+      text: "El comando ingresado no existe\n\n" \
+            "Los comandos válidos son:\n" \
+            "* Registrar cliente: /registrar nombre, direccion, codigo_postal\n" \
+            "* Registrar cadete: /registrar cadete nombre, vehiculo\n" \
+            "* Nuevo envío: /nuevo envio tamaño, direccion, codigo_postal\n" \
+            "* Consultar estado de envío: /estado envio id_envio\n" \
+            "* Asignar cadete a envío: /asignar envio\n" \
+            "* Confirmar entrega de envío: /confirmar entrega id_envio",
+      parse_mode: 'MarkdownV2'
+    )
   end
 end
