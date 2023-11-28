ae70ae37ecc0a5ef08b3c918bb256e37bd3d3d7f
Gabriel-Re <gre@fi.uba.ar>
2023-11-28 11:39:46 -0300

    Fix-13: Se agregan caracteres especiales al formateador

--- ayudantes/formateador_respuesta.rb
+++ ayudantes/formateador_respuesta.rb
@@ -2,8 +2,8 @@ class FormateadorRespuesta
   attr_accessor :texto, :cliente_id, :texto_cliente
 
   def initialize(respuesta)
-    @texto = respuesta['text'].gsub(/[-.()!]/, '\\\\\0')
+    @texto = respuesta['text'].gsub(/[-.()!#><]/, '\\\\\0')
     @cliente_id = respuesta['cliente']
-    @texto_cliente = respuesta['text_to_client']&.gsub(/[-.()!]/, '\\\\\0')
+    @texto_cliente = respuesta['text_to_client']&.gsub(/[-.()!#><]/, '\\\\\0')
   end
 end
